da1f15c36ae46a034bd2011d2a70279720abf9a7
KiritoNya <watashiwayuridaisuki@gmail.com>
2021-12-29 18:41:24 +0100

    Add others MediaType consts

--- mediaType.go
+++ mediaType.go
@@ -10,6 +10,18 @@ const (
 	//MANGA_TYPE - Japanese comics
 	MANGA_TYPE MediaType = "Manga"
 
+	//MANHUA_TYPE Chinese comics
+	MANHUA_TYPE MediaType = "Manhua"
+
+	//MANHWA_TYPE Korean comics
+	MANHWA_TYPE MediaType = "Manhwa"
+
+	//THAI_TYPE Thailand comics
+	THAI_TYPE MediaType = "Thai"
+
+	//VIETNAMITA Vietnam comics
+	VIETNAMITA MediaType = "Vietnamita"
+
 	//DOUJINSHI_TYPE - Autopublicated japanese comics
 	DOUJINSHI_TYPE MediaType = "Doujinshi"
 )
